adbaed283a2660aeac1b9e7f9a9b3a727f58e08f
Ayush <ayushpindoria2001@knigghts.ucf.edu>
2021-07-25 22:41:05 -0400

    lipupu

---
+++ src/test/java/ucf/assignments/SavingAndOpeningTest.java
@@ -0,0 +1,39 @@
+package ucf.assignments;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SavingAndOpeningTest
+{
+    private ArrayList<Inventory> list = new ArrayList<>();
+
+    @BeforeEach
+    public void start()
+    {
+        list.add(new Inventory(900.00,"A1B2C3D4E5","SAMSUNG S21 PLUS"));
+        list.add(new Inventory(400.00,"B1B2C3D4E5","SAMSUNG GEAR S3"));
+        list.add(new Inventory(600.00,"C3D4E5F6G7","APPLE WATCH"));
+    }
+
+
+    @ParameterizedTest
+    @MethodSource
+    public void Open(String test, File file) throws Exception
+    {
+        ArrayList<Inventory> opened = LoadSave.Open(file);
+        assertEquals(list,opened,test);
+    }
+
+    public static Stream<Arguments> Open()
+    {
+        return Stream.of(Arguments.of("TSV Import", new File("src\\test\\resources\\ucf\\assignments\\test.txt")), Arguments.of("HTML Import", new File("src\\test\\resources\\ucf\\assignments\\test.html")), Arguments.of("HTML Import", new File("src\\test\\resources\\ucf\\assignments\\test.json")));
+    }
+}
